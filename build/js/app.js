3f54dee98c102e605f33bc1c9357ed4a6ee2b11e
Bernowly <bernowly@gmail.com>
2016-07-11 09:30:12 -0300

    .

---
+++ build/js/app.js
@@ -0,0 +1,82 @@
+
+var db = firebase.database();
+
+function registrar() {
+    var email = document.querySelector('input[name="signup-email"]').value;
+    var password = document.querySelector('input[name="signup-password"]').value;
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+    });
+}
+
+function entrar() {
+    var email = document.querySelector('input[name="signin-email"]').value;
+    var password = document.querySelector('input[name="signin-password"]').value;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+    });
+}
+
+function entrarFacebook() {
+    console.log('Entrar Facebook');
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+}
+
+function entrarGoogle() {
+    console.log('Entrar Google');
+}
+
+function redefinirSenha() {
+    console.log('Redefinir Senha');
+}
+
+function sair() {
+    console.log('Sair');
+    firebase.auth().signOut().then(function() {
+        localStorage.clear();
+        mainView.router.loadPage('index.html');
+    }, function(error) {
+
+    });
+}
+
+
+function tirarFoto() {
+    $('#photo').trigger('touchstart');
+}
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        console.log(user);
+        localStorage.setItem('xpto-auth', user.displayName);
+        localStorage.setItem('profile-email', user.email);
+        localStorage.setItem('profile-image', user.photoURL);
+        mainView.router.loadPage('home.html');
+    } else {
+
+    }
+});
---
+++ build/js/kitchen-sink.js
@@ -0,0 +1,61 @@
+// Init App
+var myApp = new Framework7({
+    modalTitle: 'Framework7',
+    // Enable Material theme
+    material: true,
+});
+
+// Expose Internal DOM library
+var $$ = Dom7;
+
+// Add main view
+var mainView = myApp.addView('.view-main', {
+});
+
+// Show/hide preloader for remote ajax loaded pages
+// Probably should be removed on a production/local app
+$$(document).on('ajaxStart', function (e) {
+    if (e.detail.xhr.requestUrl.indexOf('autocomplete-languages.json') >= 0) {
+        // Don't show preloader for autocomplete demo requests
+        return;
+    }
+    myApp.showIndicator();
+});
+$$(document).on('ajaxComplete', function (e) {
+    if (e.detail.xhr.requestUrl.indexOf('autocomplete-languages.json') >= 0) {
+        // Don't show preloader for autocomplete demo requests
+        return;
+    }
+    myApp.hideIndicator();
+});
+
+
+$$(document).on('pageInit', function(e) {
+
+    var page = e.detail.page;
+    if (page.url === 'home.html') {
+        var cpf = localStorage.getItem('cpf');
+        var nome = localStorage.getItem('xpto-auth');
+        var img = localStorage.getItem('profile-image');
+        var email = localStorage.getItem('profile-email');
+        $('#profile-img').attr('src', img);
+        $('#profile-name').text(nome);
+        $('#profile-email').text(email);
+        // myApp.popup('.popup-ident');
+        var pnome = nome.split(" ")[0];
+        if (cpf == undefined) {
+            myApp.prompt('Olá '+pnome+'! Por favor digite o seu CPF', 'Vincular conta',
+                function(value) {
+                    localStorage.setItem('cpf', value);
+                },
+                function() {}
+            );
+        }
+    }
+    /*
+    var auth = localStorage.getItem('xpto-auth');
+    if (auth != undefined) {
+        mainView.router.loadPage('home.html');
+    }
+    */
+});
